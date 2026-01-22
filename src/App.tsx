1eafda195b0f10a4fb6217811fedbf951bb97b6c
Henrique <henriqueorsoni@hotmail.com>
2026-01-22 11:05:00 -0300

    pós formatação

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App() {
+    return (
+        <>
+            <Heading attr={123} attr2='String'>Olá Mundoss!</Heading>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error corporis voluptatum inventore cumque adipisci reprehenderit atque repudiandae dignissimos debitis quibusdam veritatis a molestias asperiores ullam libero fugiat accusamus, quos veniam.</p>
+        </>
+    );
+}
