2d7262825ee8c3dfaffa368edb529ad90186d977
Henrique <henriqueorsoni@hotmail.com>
2026-01-22 12:59:11 -0300

    componentes de titulo e container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2='String'>Olá Mundoss!</Heading>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error corporis voluptatum inventore cumque adipisci reprehenderit atque repudiandae dignissimos debitis quibusdam veritatis a molestias asperiores ullam libero fugiat accusamus, quos veniam.</p>
+            <Container>
+                <Heading>LOGO</Heading>
+            </Container>
+
+            <Container>
+                <Heading>MENU</Heading>
+            </Container>
         </>
     );
 }
